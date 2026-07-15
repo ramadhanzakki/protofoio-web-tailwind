e320d09e4a3110d82a192eb57b25c031eac5c7f8
Ramadhan Zakki <ramadhanzakki506@gmail.com>
2026-07-15 16:17:24 +0700

    feat: created fixed navbar when scroll

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
+// Hamburger
 const hamburgerBtn = document.querySelector("#hamburger");
 
 hamburgerBtn.addEventListener('click', () => {
     hamburgerBtn.classList.toggle('hamburger-active');
 })
+
+// NavBar Scroll
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const navBarTop = header.offsetTop
+
+    if (window.pageYOffset > navBarTop) {
+        header.classList.add('fixed-scroll');
+    } else {
+        header.classList.remove('fixed-scroll');
+    }
+}
